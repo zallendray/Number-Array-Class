cf230bc718f1ed45c9000f9698b7981b55331572
zallendray <kn4jna@gmail.com>
2026-03-17 20:07:25 -0400

    Wrote constructor and destructor functions, as well as setnumber and getNumber functions.

--- Number Array Class/NumberArray.cpp
+++ Number Array Class/NumberArray.cpp
@@ -4,22 +4,38 @@
 
 NumberArray::NumberArray(int size)
 {
+	NumberArray::data = new double[size];
 
+	for (int i = 0; i < size; i++) 
+	{
+		NumberArray::data[i] = 0;
+	}
 }
 
 NumberArray::~NumberArray()
 {
-
+	delete[] NumberArray::data;
+	std::cout << std::endl << "Memory successfully deallocated.";
 }
 
 void NumberArray::setNumber(int index, double value)
 {
-
+	if (index > -1 and index < NumberArray::size) {
+		NumberArray::data[index] = value;
+	}
+	else {
+		std::cout << std::endl << "The index is out of bounds.";
+	}
 }
 
 double NumberArray::getNumber(int index) const
 {
-
+	if (index > -1 and index < NumberArray::size) {
+		return NumberArray::data[index];
+	}
+	else {
+		return 0;
+	}
 }
 
 double NumberArray::getMin() const
