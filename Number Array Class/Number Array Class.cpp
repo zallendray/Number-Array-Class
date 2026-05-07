e809c6027764afd2d011368dab191f70a70071cc
zallendray <kn4jna@gmail.com>
2026-05-07 18:30:40 -0400

    Made template functions inline, added exception handling, removed NumberArray.cpp, modified test program to demonstrate changes

--- Number Array Class/Number Array Class.cpp
+++ Number Array Class/Number Array Class.cpp
@@ -6,7 +6,8 @@
 
 int main()
 {
-    NumberArray NA;
+    std::cout << "---NUMBERARRAY TESTS WITH DOUBLE---" << std::endl;
+    NumberArray<double> NA;
 
     std::cout << "Default Array:";
     NA.print();
@@ -16,13 +17,18 @@ int main()
     std::uniform_int_distribution<int>dist(0, 1000);
 
 
-    for (int i = 0; i < NumberArray::size; i++) {
+    for (int i = 0; i < NumberArray<double>::size; i++) {
         int x = dist(randev);
 
         double y = x;
         y /= 1000;
 
-        NA.setNumber(i, y);
+        try {
+            NA.setNumber(i, y);
+        }
+        catch (const std::out_of_range& e) {
+            std::cerr << "The requested action could not be performed." << std::endl;
+        }
     }
     
     std::cout << std::endl << std::endl << "Random Array:";
@@ -31,15 +37,28 @@ int main()
     std::cout << std::endl << std::endl << "Average Value: " << std::endl << NA.getAverage() << std::endl;
     std::cout << std::endl << std::endl << "Max Value: " << std::endl << NA.getMax() << std::endl;
     std::cout << std::endl << std::endl << "Min Value: " << std::endl << NA.getMin() << std::endl;
-    std::cout << std::endl << std::endl << "Number 5: " << std::endl << NA.getNumber(4) << std::endl;
-    std::cout << std::endl << std::endl << "Number 41: " << std::endl << NA.getNumber(40) << std::endl;
+
+    std::cout << std::endl << std::endl << "Number 5: " << std::endl;
+    try {
+        std::cout << NA.getNumber(4) << std::endl;
+    }
+    catch (const std::out_of_range& e) {
+        std::cerr << "The requested action could not be performed." << std::endl;
+    }
+    std::cout << std::endl << std::endl << "Number 41: " << std::endl;
+    try {
+        std::cout << NA.getNumber(40) << std::endl;
+    }
+    catch (const std::out_of_range& e) {
+        std::cerr << "The requested action could not be performed." << std::endl;
+    }
 
 
-    std::cout << "Copy Contructor and Overloaded Assignment Operator Tests:" << std::endl << std::endl;
+    std::cout << std::endl << "Copy Contructor and Overloaded Assignment Operator Tests:" << std::endl << std::endl;
 
-    NumberArray a;
+    NumberArray<double> a;
 
-    for (int i = 0; i < NumberArray::size; i++) {
+    for (int i = 0; i < NumberArray<double>::size; i++) {
         a.setNumber(i, i);
     }
 
@@ -55,7 +74,7 @@ int main()
     std::cout << std::endl << "ARRAY B:" << std::endl;
     b.print();
 
-    for (int i = 0; i < NumberArray::size; i++) {
+    for (int i = 0; i < NumberArray<double>::size; i++) {
         b.setNumber(i, i+20);
     }
 
@@ -65,5 +84,81 @@ int main()
     b.print();
     std::cout << std::endl << "ASSIGNED ARRAY A:" << std::endl;
     a.print();
+
+
+
+
+    std::cout << std::endl << std::endl <<"---NUMBERARRAY TESTS WITH INT---" << std::endl;
+    NumberArray<int> NA_INT;
+
+    std::cout << "Default Array:";
+    NA_INT.print();
+
+    for (int i = 0; i < NumberArray<int>::size; i++) {
+        int x = dist(randev);
+
+        double y = x;
+
+        try {
+            NA_INT.setNumber(i, y);
+        }
+        catch (const std::out_of_range& e) {
+            std::cerr << "The requested action could not be performed." << std::endl;
+        }
+    }
+
+    std::cout << std::endl << std::endl << "Random Array:";
+    NA_INT.print();
+
+    std::cout << std::endl << std::endl << "Average Value: " << std::endl << NA_INT.getAverage() << std::endl;
+    std::cout << std::endl << std::endl << "Max Value: " << std::endl << NA_INT.getMax() << std::endl;
+    std::cout << std::endl << std::endl << "Min Value: " << std::endl << NA_INT.getMin() << std::endl;
+
+    std::cout << std::endl << std::endl << "Number 5: " << std::endl;
+    try {
+        std::cout << NA_INT.getNumber(4) << std::endl;
+    }
+    catch (const std::out_of_range& e) {
+        std::cerr << "The requested action could not be performed." << std::endl;
+    }
+    std::cout << std::endl << std::endl << "Number 41: " << std::endl;
+    try {
+        std::cout << NA_INT.getNumber(40) << std::endl;
+    }
+    catch (const std::out_of_range& e) {
+        std::cerr << "The requested action could not be performed." << std::endl;
+    }
+
+
+    std::cout << std::endl << "Copy Contructor and Overloaded Assignment Operator Tests:" << std::endl << std::endl;
+
+    NumberArray<int> a_int;
+
+    for (int i = 0; i < NumberArray<int>::size; i++) {
+        a_int.setNumber(i, i);
+    }
+
+    NumberArray b_int(a_int);
+    std::cout << std::endl << "ARRAY A:" << std::endl;
+    a_int.print();
+    std::cout << std::endl << "ARRAY B:" << std::endl;
+    b_int.print();
+
+    a_int.setNumber(3, 8);
+    std::cout << std::endl << "MODIFIED ARRAY A:" << std::endl;
+    a_int.print();
+    std::cout << std::endl << "ARRAY B:" << std::endl;
+    b_int.print();
+
+    for (int i = 0; i < NumberArray<int>::size; i++) {
+        b_int.setNumber(i, i + 20);
+    }
+
+
+    a_int = a_int;
+    std::cout << std::endl << "NEW ARRAY B:" << std::endl;
+    b_int.print();
+    std::cout << std::endl << "ASSIGNED ARRAY A:" << std::endl;
+    a_int.print();
 }
 
--- Number Array Class/NumberArray.h
+++ Number Array Class/NumberArray.h
@@ -1,6 +1,9 @@
 #ifndef NUMBERARRAY_H
 #define NUMBERARRAY_H
 
+#include <iostream>
+#include <stdexcept>
+
 template<typename T>
 class NumberArray
 {
@@ -10,25 +13,106 @@ private:
 public:
 	static const int size = 9;
 
-	NumberArray(int array_size = size);
+	inline NumberArray(int array_size = size) {
+		NumberArray::data = new T[size];
 
-	~NumberArray();
+		for (int i = 0; i < size; i++)
+		{
+			NumberArray::data[i] = 0;
+		}
+	}
 
-	void setNumber(int index, T value);
+	inline ~NumberArray() {
+		delete[] NumberArray::data;
+		std::cout << std::endl << "Memory successfully deallocated.";
+	}
 
-	T getNumber(int index) const;
+	inline void setNumber(int index, T value) {
+		if (index > -1 and index < NumberArray::size) {
+			NumberArray::data[index] = value;
+		}
+		else {
+			throw std::out_of_range("The specified index is out of range");
+			//std::cout << std::endl << "The index is out of bounds.";
+		}
+	}
 
-	T getMin() const;
+	inline T getNumber(int index) const {
+		if (index > -1 and index < NumberArray::size) {
+			return NumberArray::data[index];
+		}
+		else {
+			throw std::out_of_range("The specified index is out of range");
+			//return 0;
+		}
+	}
 
-	T getMax() const;
+	inline T getMin() const {
+		T min = NumberArray::data[0];
 
-	T getAverage() const;
+		for (int i = 0; i < NumberArray::size; i++)
+		{
+			if (NumberArray::data[i] < min) {
+				min = NumberArray::data[i];
+			}
+		}
+		return min;
+	}
 
-	void print() const;
+	inline T getMax() const {
+		T max = NumberArray::data[0];
 
-	NumberArray(const NumberArray& other);
+		for (int i = 0; i < NumberArray::size; i++)
+		{
+			if (NumberArray::data[i] > max) {
+				max = NumberArray::data[i];
+			}
+		}
+		return max;
+	}
 
-	NumberArray& operator=(const NumberArray& other);
+	inline T getAverage() const {
+		T average = 0;
+
+		for (int i = 0; i < NumberArray::size; i++)
+		{
+			average += NumberArray::data[i];
+		}
+		average /= NumberArray::size;
+
+		return average;
+
+	}
+
+	inline void print() const {
+		std::cout << std::endl;
+
+		for (int i = 0; i < NumberArray::size; i++)
+		{
+			std::cout << std::endl << NumberArray::data[i];
+		}
+	}
+
+	inline NumberArray(const NumberArray& other) {
+		data = new T[size];
+		for (int i = 0; i < size; i++)
+		{
+			data[i] = other.data[i];
+		}
+	}
+
+	inline NumberArray& operator=(const NumberArray& other) {
+		if (this != &other)
+		{
+			delete[] data;
+			data = new T[size];
+			for (int i = 0; i < size; i++)
+			{
+				data[i] = other.data[i];
+			}
+		}
+		return *this;
+	}
 };
 
 #endif
